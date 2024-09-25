a485aff20ffaca0871de5b75814eb9d8df0e0158
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-09-25 17:38:02 +0100

    init

---
+++ server.js
@@ -0,0 +1,9 @@
+/** Server for bookstore. */
+
+
+const app = require("./app");
+
+app.listen(3000, () => {
+  console.log(`Server starting on port 3000`);
+  console.log(`http://localhost:3000`);
+});
